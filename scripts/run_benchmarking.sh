c825d952773d6769e8664d4d679f65ed2eb41b16
Pedro Pons <pedro-henrique.pons-fiorentin@grenoble-inp.org>
2022-11-10 16:41:00 +0100

    Readded date to benchmark script

--- scripts/run_benchmarking.sh
+++ scripts/run_benchmarking.sh
@@ -1,11 +1,15 @@
 OUTPUT_DIRECTORY=data/my_laptop
 mkdir -p $OUTPUT_DIRECTORY
-OUTPUT_FILE=$OUTPUT_DIRECTORY/recent_measurements.txt
-OUTPUT_CSV=$OUTPUT_DIRECTORY/recent_measurements.csv
+
+OUTPUT_SUFFIX=measurements_`date +%R`
+OUTPUT_SUFFIX="$( echo "$OUTPUT_SUFFIX" | tr ':' '_' )"
+
+OUTPUT_FILE=$OUTPUT_DIRECTORY/$OUTPUT_SUFFIX.txt
+OUTPUT_CSV=$OUTPUT_DIRECTORY/$OUTPUT_SUFFIX.csv
 
 touch $OUTPUT_FILE
-for i in 100 1000 10000 100000 1000000; do
-    for rep in `seq 1 5`; do
+for i in 100 1000 10000 100000 500000 1000000 2000000; do
+    for rep in `seq 1 10`; do
         echo "Size: $i" >> $OUTPUT_FILE;
         ./src/parallelQuicksort $i >> $OUTPUT_FILE;
     done ;
