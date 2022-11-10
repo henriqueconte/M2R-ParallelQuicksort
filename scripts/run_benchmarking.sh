97723ce41f1f9c234b6b2c61b294ac77f264392b
Pedro Pons <pedro-henrique.pons-fiorentin@grenoble-inp.org>
2022-11-10 16:46:57 +0100

    Code cleanup

--- scripts/run_benchmarking.sh
+++ scripts/run_benchmarking.sh
@@ -4,15 +4,15 @@ mkdir -p $OUTPUT_DIRECTORY
 OUTPUT_SUFFIX=measurements_`date +%R`
 OUTPUT_SUFFIX="$( echo "$OUTPUT_SUFFIX" | tr ':' '_' )"
 
-OUTPUT_FILE=$OUTPUT_DIRECTORY/$OUTPUT_SUFFIX.txt
+OUTPUT_TXT=$OUTPUT_DIRECTORY/$OUTPUT_SUFFIX.txt
 OUTPUT_CSV=$OUTPUT_DIRECTORY/$OUTPUT_SUFFIX.csv
 
-touch $OUTPUT_FILE
+touch $OUTPUT_TXT
 for i in 100 1000 10000 100000 500000 1000000 2000000; do
     for rep in `seq 1 10`; do
-        echo "Size: $i" >> $OUTPUT_FILE;
-        ./src/parallelQuicksort $i >> $OUTPUT_FILE;
+        echo "Size: $i" >> $OUTPUT_TXT;
+        ./src/parallelQuicksort $i >> $OUTPUT_TXT;
     done ;
 done
 
-perl scripts/csv_quicksort_extractor.pl < $OUTPUT_FILE > $OUTPUT_CSV
+perl scripts/csv_quicksort_extractor.pl < $OUTPUT_TXT > $OUTPUT_CSV
